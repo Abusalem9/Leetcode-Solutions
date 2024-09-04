86496cb1fd1717f6b3fd108139c1ca3b31bb6d85
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-09-04 06:55:23 +0530

    Time: 2 ms (99.65%), Space: 43.5 MB (24.34%) - LeetHub

--- 1547-destination-city/1547-destination-city.java
+++ 1547-destination-city/1547-destination-city.java
@@ -1,17 +1,12 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        LinkedHashSet<String> set = new LinkedHashSet<>();
-        HashMap<String,String> map = new HashMap<>();
-
-        for(List<String> path : paths){
-            set.add(path.get(0));
-            set.add(path.get(1));
-            map.put(path.get(0),path.get(1));
+        Set<String> sourceSet = new HashSet<>();
+        for (List<String> pair: paths) {       
+            sourceSet.add(pair.get(0));
         }
-
-        for(String s : set){
-            if(!map.containsKey(s)){
-                return s;
+        for (List<String> pair: paths) {
+            if (!sourceSet.contains(pair.get(1))) {
+                return pair.get(1);
             }
         }
         return null;

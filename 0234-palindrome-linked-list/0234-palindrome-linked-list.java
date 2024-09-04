3e1301bc9f43d68a1386d301984a3a263a6a2799
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-09-04 06:04:19 +0530

    Time: 3 ms (99.65%), Space: 69.2 MB (12.29%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // Move slow to the middle and reverse the first half of the list
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        // Adjust pointers if the number of nodes is odd
+        ListNode secondHalfStart = fast == null ? slow : slow.next;
+
+        // Compare nodes in the first half with nodes in the second half
+        ListNode p1 = prev;
+        ListNode p2 = secondHalfStart;
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+}

ebc69d734868fcc1a436860988954384ef7ca86f
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-10-28 09:40:46 -0700

    Time: 25 ms (76.23%), Space: 40.4 MB (28.37%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.java
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -2,22 +2,23 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
 
+    public int firstBadVersion(int n) {
+        
         int left = 1;
         int right = n;
 
-        while (left <= right) {
-
+        while(left <= right){
             int mid = left + (right - left) / 2;
 
-            if (isBadVersion(mid)) {
+            if(isBadVersion(mid)){
                 right = mid - 1;
-            } else {
+            }
+            else{
                 left = mid + 1;
             }
         }
 
-        return right + 1;
+        return left;
     }
 }

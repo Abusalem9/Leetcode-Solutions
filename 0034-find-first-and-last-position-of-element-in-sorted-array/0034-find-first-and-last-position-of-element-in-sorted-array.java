d535e1568d6f1e6d376089bd976fd0825cc57ab9
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-10-28 10:29:25 -0700

    Time: 0 ms (100%), Space: 45.8 MB (50.18%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -4,9 +4,7 @@ class Solution {
         int left = 0;
         int right = nums.length - 1;
 
-        int[] ans = new int[2];
-        Arrays.fill(ans, -1);
-
+        int[] ans = { -1, -1 };
         while (left <= right) {
 
             int mid = left + (right - left) / 2;

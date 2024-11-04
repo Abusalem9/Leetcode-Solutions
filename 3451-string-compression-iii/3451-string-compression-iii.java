3aa2c0a529aa0a24a9e6fff93984e98112b77c17
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-11-04 03:34:44 -0800

    Time: 16 ms (94.92%), Space: 45.4 MB (96.04%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,18 +1,19 @@
 class Solution {
     public String compressedString(String word) {
+        if (word == null || word.isEmpty()) {
+            return "";
+        }
 
-        int count = 0;
-
+        int count = 1;
         char last = word.charAt(0);
-
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 1; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if (i > 0 && last == ch) {
+            if (ch == last) {
                 count++;
                 if (count == 9) {
-                    result.append('9').append(ch);
+                    result.append('9').append(last);
                     count = 0;
                 }
             } else {

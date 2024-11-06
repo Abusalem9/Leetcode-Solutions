69291beaf20526f58d2c77c7a8b1deb98d910b66
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2024-11-05 18:54:06 -0800

    Time: 1 ms (100%), Space: 44.2 MB (61.64%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -3,23 +3,34 @@ class Solution {
         int n = nums.length;
 
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                int setBitsCurrent = Integer.bitCount(nums[j]);
-                int setBitsNext = Integer.bitCount(nums[j + 1]);
+            if (nums[i] <= nums[i + 1])
+                continue;
+            else {
+                if (Integer.bitCount(nums[i]) == Integer.bitCount(nums[i + 1])) {
 
-                if (setBitsCurrent == setBitsNext && nums[j] > nums[j + 1]) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+                    int temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                } else
+                    return false;
             }
         }
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i - 1] > nums[i]) {
-                return false;
+        for (int i = n - 1; i >= 1; i--) {
+            if (nums[i] >= nums[i - 1])
+                continue;
+            else {
+
+                if (Integer.bitCount(nums[i]) == Integer.bitCount(nums[i - 1])) {
+
+                    int temp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = temp;
+                } else
+                    return false;
             }
         }
+
         return true;
     }
 }

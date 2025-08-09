caac09912acf58d6f8051dd3f4434a4e8034346c
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2025-08-10 00:51:07 +0530

    Time: 0 ms (100%), Space: 41 MB (36.36%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        
-        if(n == 0){
-            return false;
-        }
 
-        if((n & n - 1) == 0){
+        if (n > 0 && (n & (n - 1)) == 0) {
             return true;
         }
-
         return false;
     }
 }

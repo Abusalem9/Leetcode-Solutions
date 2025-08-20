e3b688eaa5e61c4e20ffedef2deccd9cc64d9d5c
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2025-08-20 21:52:12 +0530

    Time: 7 ms (40.76%), Space: 55.5 MB (33.3%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,34 +1,23 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-
         int n = matrix.length;
         int m = matrix[0].length;
-        int[][] visited = new int[n][m];
+        int[][] dp = new int[n][m];
         int count = 0;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                count += dfs(n, m, matrix, visited, i, j);
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = 1 + Math.min(dp[i - 1][j],
+                                       Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    }
+                    count += dp[i][j];
+                }
             }
         }
-
         return count;
-
-    }
-
-    public int dfs(int n, int m, int[][] matrix, int[][] visited, int i, int j) {
-        if (i == n || j == m || matrix[i][j] == 0) {
-            return 0;
-        }
-
-        if(visited[i][j] != 0){
-            return visited[i][j];
-        }
-
-        visited[i][j] = 1
-                + Math.min(dfs(n, m, matrix, visited, i + 1, j), Math.min(dfs(n, m, matrix, visited, i + 1, j + 1),
-                        dfs(n, m, matrix, visited, i, j + 1)));
-
-        return visited[i][j];                    
     }
 }

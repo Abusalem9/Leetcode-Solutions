d91a344763fce0a265729fb4ae080c4b300614d5
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2025-09-03 06:28:54 +0530

    Time: 0 ms (100%), Space: 41.3 MB (17.01%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+        backTrack(k, n, ans, new ArrayList<>(), 1, 0);
+        return ans;
+    }
+
+    public void backTrack(int k, int n, List<List<Integer>> ans, List<Integer> temp, int start, int sum) {
+        if (sum == n && temp.size() == k) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        if (sum > n || temp.size() > k || start > 9) {
+            return;
+        }
+        if (temp.size() + (9 - start + 1) < k) {
+            return;
+        }
+
+        temp.add(start);
+        backTrack(k, n, ans, temp, start + 1, sum + start);
+        temp.remove(temp.size() - 1);
+
+        backTrack(k, n, ans, temp, start + 1, sum);
+    }
+}

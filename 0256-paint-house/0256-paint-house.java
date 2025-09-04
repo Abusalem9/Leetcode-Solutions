b74deb88beb4f6c359dbdb9348f3d8f20c29d4ff
Abusalem Mangalwedhe <97865831+Abusalem9@users.noreply.github.com>
2025-09-04 07:50:01 +0530

    Time: 1 ms (87.4%), Space: 43 MB (47.59%) - LeetHub

---
+++ 0256-paint-house/0256-paint-house.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCost(int[][] costs) {
+        int red = costs[0][0];
+        int blue = costs[0][1];
+        int green = costs[0][2];
+
+        for (int i = 1; i < costs.length; i++) {
+            int newRed = costs[i][0] + Math.min(blue, green);
+            int newBlue = costs[i][1] + Math.min(red, green);
+            int newGreen = costs[i][2] + Math.min(blue, red);
+            red = newRed;
+            blue = newBlue;
+            green = newGreen;
+        }
+
+        return Math.min(red, Math.min(blue, green));
+    }
+}

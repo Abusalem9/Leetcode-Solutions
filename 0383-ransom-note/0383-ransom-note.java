16028c3d6b20b3df1af1bfbe902d17ce70f846e7
Abusalem M <97865831+Abusalem9@users.noreply.github.com>
2026-05-05 21:07:36 +0530

    Time: 2 ms (59.04%), Space: 46.2 MB (89.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,21 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        Map<Character, Integer> map = new HashMap<>();
+        int[] ca = new int[26];
 
         for (char ch : magazine.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            ca[ch - 'a']++;
         }
 
         for (char ch : ransomNote.toCharArray()) {
-            if (map.containsKey(ch)) {
-                int temp = map.get(ch);
-                if (temp < 1)
-                    return false;
-                map.put(ch, temp - 1);
-            } else {
+            if (ca[ch - 'a'] < 1)
                 return false;
-            }
+            ca[ch - 'a']--;
         }
         return true;
     }
